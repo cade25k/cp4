6d8ee9bb28f1f95d4de784e2333e59411a83dcd8
cade25k <77406426+cade25k@users.noreply.github.com>
2021-04-06 13:04:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -165,4 +165,4 @@ app.delete('/api/completed/:id', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3005, () => console.log('Server listening on port 3005!'));
